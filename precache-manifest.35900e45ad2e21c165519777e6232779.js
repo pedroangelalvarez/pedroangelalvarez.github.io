c2246e1f90f1eb9c562efd049e636a3bc8555c2a
Pedro Alvarez <pedroangel.alvarez24@gmail.com>
2020-11-07 00:08:21 -0500

    mixed signals

---
+++ precache-manifest.35900e45ad2e21c165519777e6232779.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4d23b4c6998a2c357ae4eb9275289c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02eacbae15c710011e31",
+    "url": "/static/css/main.6969711f.chunk.css"
+  },
+  {
+    "revision": "cbfd5eecf3f349c0be93",
+    "url": "/static/js/2.453fc63e.chunk.js"
+  },
+  {
+    "revision": "02eacbae15c710011e31",
+    "url": "/static/js/main.f6515724.chunk.js"
+  },
+  {
+    "revision": "11466a4ea122d4365add",
+    "url": "/static/js/runtime-main.f9b6ada7.js"
+  }
+]);
